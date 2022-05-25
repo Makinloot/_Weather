ac1c0435d3385646729c33c0bd470654bac734e0
Tornike Epitashvili <itorn9777@gmail.com>
2022-05-25 04:22:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening at ${PORT}`));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
