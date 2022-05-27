091447df35cfc829b83089351633821000d999d4
Tornike Epitashvili <itorn9777@gmail.com>
2022-05-27 15:45:44 +0400

    updated slider with swiper.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening at ${PORT}`));
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
-require('dotenv').config();
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+require("dotenv").config();
 
-app.post('/api', async (request, response) => {
-    const API_KEY = process.env.API_KEY;
-    const data = request.body;
-    const lat = data.latitude;
-    const lon = data.longitude;
-    const weather_url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${lon}&days=10`;
+app.post("/api", async (request, response) => {
+  const API_KEY = process.env.API_KEY;
+  const data = request.body;
+  const lat = data.latitude;
+  const lon = data.longitude;
+  const weather_url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${lon}&days=10`;
+  const weather_res = await fetch(weather_url);
+  const weather_data = await weather_res.json();
 
-    const weather_res = await fetch(weather_url);
-    const weather_data = await weather_res.json();
-
-    response.json(weather_data);
-})
+  response.json(weather_data);
+});
--- public/main.js
+++ public/main.js
@@ -1,178 +1,122 @@
-navigator.geolocation.getCurrentPosition(async position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+navigator.geolocation.getCurrentPosition(async (position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
 
-    const locationData = {
-        latitude: lat,
-        longitude: lon
-    }
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(locationData)
-    }
+  const locationData = {
+    latitude: lat,
+    longitude: lon,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(locationData),
+  };
 
-    const res = await fetch('/api', options);
-    const data = await res.json();
-    console.log(data);
-    headerData(data);
-    hourlyData(data);
-    forecastData(data);
-    fetchDetails(data);
-
-    hourlyDataSlider();
+  const res = await fetch("/api", options);
+  const data = await res.json();
+  console.log(data);
+  headerData(data);
+  hourlyData(data);
+  forecastData(data);
+  fetchAstro(data);
 });
 
 // pulls data from api for header
 function headerData(data) {
-    const location = data.location.name;
-    const temperature = data.current.temp_c;
-    const condition = data.current.condition.text;
-    const maxTemp = data.forecast.forecastday[0].day.maxtemp_c;
-    const minTemp = data.forecast.forecastday[0].day.mintemp_c;
-    const degreeSpan = document.createElement('span');
+  const location = data.location.name;
+  const temperature = data.current.temp_c;
+  const condition = data.current.condition.text;
+  const maxTemp = data.forecast.forecastday[0].day.maxtemp_c;
+  const minTemp = data.forecast.forecastday[0].day.mintemp_c;
+  const degreeSpan = document.createElement("span");
 
-    degreeSpan.innerText = '°';
+  degreeSpan.innerText = "°";
 
-    document.getElementById('header-location').textContent = location;
-    document.getElementById('header-temp').append(temperature, degreeSpan)
-    document.getElementById('header-condition').textContent = condition;
-    document.getElementById('header-highest').textContent = maxTemp;
-    document.getElementById('header-lowest').textContent = minTemp;
+  document.getElementById("header-location").textContent = location;
+  document.getElementById("header-temp").append(temperature, degreeSpan);
+  document.getElementById("header-condition").textContent = condition;
+  document.getElementById("header-highest").textContent = maxTemp;
+  document.getElementById("header-lowest").textContent = minTemp;
 }
 
 // pulls data from api and displays it in slider
 function hourlyData(data) {
-    const path = data.forecast.forecastday[0].hour;
+  const path = data.forecast.forecastday[0].hour;
 
-    path.forEach( item => {
-        const slider = document.getElementById('slider-item');
-        const time = item.time.split(' ')[1].split(':')[0];
-        const hourlyCondition = item.condition.icon;
-        const hourlyTemp = Math.floor(item.temp_c);
-        
-        const root = document.createElement('div');
-        root.classList.add('slider__item', 'flex');
-        root.innerHTML = `
+  path.forEach((item) => {
+    const slider = document.getElementById("slider-item");
+    const time = item.time.split(" ")[1].split(":")[0];
+    const hourlyCondition = item.condition.icon;
+    const hourlyTemp = Math.floor(item.temp_c);
+
+    const root = document.createElement("div");
+    // root.classList.add('slider__item', 'flex');
+    root.classList.add("swiper-slide", "flex");
+    root.innerHTML = `
             <p>${time}</p>
             <img class='hourly__icon' src=${hourlyCondition} alt='weather condition'>
             <strong class='hourly__temp'>${hourlyTemp}<span class='hourly__degree'>°</span></strong>
         `;
-        slider.append(root);
-    })
+    slider.append(root);
+  });
 }
 
-// slider for hourly data
-function hourlyDataSlider() {
-    const slider = document.getElementById('slider');
-    const sliderItem = document.getElementById('slider-item');
-    let isDown = false;
-    let startX;
-    let x;
-    
-    window.addEventListener('mouseup', () => {
-        isDown = false;
-    })
-    
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        startX = e.offsetX - sliderItem.offsetLeft;
-        slider.style.cursor = 'grabbing';
-    });
-    
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.style.cursor = 'grab';
-    });
-    
-    slider.addEventListener('mouseenter', () => {
-        slider.style.cursor = 'grab';
-    })
-    
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-    });
-    
-    slider.addEventListener('mousemove', (e) => {
-        if(!isDown) return;
-        e.preventDefault();
-        x = e.offsetX;
-    
-        sliderItem.style.left = `${x - startX}px`;
-        boundary();
-    });
-    
-    slider.addEventListener('touchstart', (e) => {
-        isDown = true;
-        startX = e.targetTouches[0].clientX - sliderItem.offsetLeft;
-    }, {passive: true});
-    
-    slider.addEventListener('touchmove', (e) => {
-        if(!isDown) return;
-        x = e.targetTouches[0].clientX;
-    
-        sliderItem.style.left = `${x - startX}px`
-        boundary();
-    }, {passive: true});
+function fetchAstro(data) {
+  const currentAstro = document.getElementById("current-astro-text");
+  const currentAstroIcon = document.getElementById("current-astro-icon");
+  const pastAstro = document.getElementById("past-astro-text");
+  const sunriseText = document.getElementById("sunrise");
+  const sunsetText = document.getElementById("sunset");
+  const astroPath = data.forecast.forecastday[0].astro;
+  const sunrise = astroPath.sunrise;
+  const sunset = astroPath.sunset;
+  let time = new Date().getHours();
 
-    function boundary() {
-        let outer = slider.getBoundingClientRect();
-        let inner = sliderItem.getBoundingClientRect();
-    
-        if(parseInt(sliderItem.style.left) > 0) {
-            sliderItem.style.left = '0px';
-        } else if(inner.right < outer.right) {
-            sliderItem.style.left = `-${inner.width - outer.width}px`;
-        }
-    }
+  if (time >= sunrise.split(":")[0]) {
+    currentAstro.textContent = sunset;
+    pastAstro.textContent = sunrise;
+    sunriseText.textContent = "SUNSET";
+    sunsetText.textContent = "SUNRISE";
+    currentAstroIcon.setAttribute("src", "../images/sunrise.png");
+  } else if (time <= sunrise.split(":")[0]) {
+    currentAstro.textContent = sunrise;
+    pastAstro.textContent = sunset;
+    sunriseText.textContent = "SUNRISE";
+    sunsetText.textContent = "SUNSET";
+    currentAstroIcon.setAttribute("src", "../images/sunrise.png");
+  }
+
+  document.getElementById(
+    "feels-like-temp"
+  ).textContent = `${data.current.feelslike_c}°`;
 }
-
-function fetchDetails(data) {
-    const wrapper = document.getElementById('sunrise-wrapper');
-    const astroPath = data.forecast.forecastday[0].astro;
-    const sunrise = astroPath.sunrise;
-    const sunset = astroPath.sunset;
-
-    
-}
-
-
-
-
-
-
-
-
-
-
-
 
 // pulls data from api for 3 day forecast
 function forecastData(data) {
-    const forecastWrapper = document.getElementById('forecast-wrapper');
-    forecastWrapper.innerHTML = '';
+  const forecastWrapper = document.getElementById("forecast-wrapper");
+  forecastWrapper.innerHTML = "";
 
-    let forecastPath = data.forecast.forecastday;
-    for(let i = 0; i < forecastPath.length; i++) {
-        let iconPath = forecastPath[i].day.condition.icon;
-        let minPath = Math.floor(forecastPath[i].day.mintemp_c);
-        let maxPath = Math.floor(forecastPath[i].day.maxtemp_c);
-        let fulldate = new Date()
-        fulldate.setDate(fulldate.getDate() + i)
-        const currDay = fulldate.toLocaleString('en-us', {weekday:'long'});
+  let forecastPath = data.forecast.forecastday;
+  for (let i = 0; i < forecastPath.length; i++) {
+    let iconPath = forecastPath[i].day.condition.icon;
+    let minPath = Math.floor(forecastPath[i].day.mintemp_c);
+    let maxPath = Math.floor(forecastPath[i].day.maxtemp_c);
+    let fulldate = new Date();
+    fulldate.setDate(fulldate.getDate() + i);
+    const currDay = fulldate.toLocaleString("en-us", { weekday: "long" });
 
-        const root = document.createElement('div');
-        root.classList.add('forecast__row');
-        root.innerHTML = `
+    const root = document.createElement("div");
+    root.classList.add("forecast__row", "flex");
+    root.innerHTML = `
             <p class='row__day'>${currDay}</p>
             <img class='row__icon' src=${iconPath} alt='condition img'>
             <div class='min-max__row flex'>
                 <p class='row__max-temp'><span>H:</span>${maxPath}°</p>
                 <p class='row__min-temp'><span>H:</span>${minPath}°</p>
             </div>
-        `
-        forecastWrapper.append(root);
-    }
+        `;
+    forecastWrapper.append(root);
+  }
 }
