80016aa05b00248756def88f8312fcbd34e85be4
Tornike Epitashvili <itorn9777@gmail.com>
2022-05-31 02:48:04 +0400

    format code

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening at ${PORT}`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 require("dotenv").config();
-
 // sending weather data to user
 app.post("/api", async (request, response) => {
   const API_KEY = process.env.API_KEY;
@@ -20,7 +18,6 @@ app.post("/api", async (request, response) => {
 
   response.json(weather_data);
 });
-
 // sending api and map keys
 app.post("/token", async (request, response) => {
   const mapKey = process.env.MAP_TOKEN;
--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
+// import jeaflet.js to main.js
 import "./lib/leaflet.js";
+// call weather
 fetchApi();
-
 async function fetchApi() {
   // Making request for API key and MAP key
   const tokens = {
@@ -18,7 +19,6 @@ async function fetchApi() {
   const token_data = await token_res.json();
   const IP_KEY = token_data.ip_token;
   const MAP_KEY = token_data.map_token;
-
   // fetching data for IP API
   const ip_res = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${IP_KEY}`);
   const ip_data = await ip_res.json();
@@ -39,8 +39,7 @@ async function fetchApi() {
   // fetching data of weather API from server
   const res = await fetch("/api", options);
   const data = await res.json();
-  console.log(data);
-
+  // call weather functions
   headerData(data);
   hourlyData(data);
   forecastData(data);
@@ -49,36 +48,34 @@ async function fetchApi() {
   rainData(data);
   changeBackground(data);
 }
-
 // displays data from api for header
 function headerData(data) {
+  // header data path
   const location = data.location.name;
   const temperature = data.current.temp_c;
   const condition = data.current.condition.text;
   const maxTemp = data.forecast.forecastday[0].day.maxtemp_c;
   const minTemp = data.forecast.forecastday[0].day.mintemp_c;
   const degreeSpan = document.createElement("span");
-
+  // creates text content for header data
   degreeSpan.innerText = "°";
-
   document.getElementById("header-location").textContent = location;
   document.getElementById("header-temp").append(temperature, degreeSpan);
   document.getElementById("header-condition").textContent = condition;
   document.getElementById("header-highest").textContent = maxTemp;
   document.getElementById("header-lowest").textContent = minTemp;
 }
-
 // pulls data from api and displays it in slider
 function hourlyData(data) {
   const path = data.forecast.forecastday[0].hour;
-
   // making slider items in HTML format and putting them in slider-item wrapper
   path.forEach((item) => {
+    // slider data path
     const slider = document.getElementById("slider-item");
     const time = item.time.split(" ")[1].split(":")[0];
     const hourlyCondition = item.condition.icon;
     const hourlyTemp = Math.floor(item.temp_c);
-
+    // Creates HTML for slider items and appends it slider
     const root = document.createElement("div");
     root.classList.add("swiper-slide", "flex");
     root.innerHTML = `
@@ -89,9 +86,9 @@ function hourlyData(data) {
     slider.append(root);
   });
 }
-
 // displays astro data from API
 function fetchAstro(data) {
+  // astro HTML elements & path
   const currentAstro = document.getElementById("current-astro-text");
   const currentAstroIcon = document.getElementById("current-astro-icon");
   const pastAstro = document.getElementById("past-astro-text");
@@ -102,15 +99,13 @@ function fetchAstro(data) {
   const sunrise = astroPath.sunrise;
   const sunset = astroPath.sunset;
   let time = new Date().getHours();
-
   // create progress bar
   const progressBar = document.getElementById('progress-bar');
-  
   // move progress bar according to time
   progressBar.style.left = time * 4.1 + '%';  
-
   // determines to display SUNRISE or SUNSET first, depending on time of the day.
   if (time >= sunrise.split(":")[0]) {
+    // if day
     currentAstro.textContent = sunset;
     pastAstro.textContent = sunrise;
     sunriseText.textContent = "SUNSET";
@@ -119,6 +114,7 @@ function fetchAstro(data) {
     barIcon.setAttribute('src', '../images/sun.png');
     barIcon.setAttribute('alt', 'sun icon');
   } else if (time <= sunrise.split(":")[0]) {
+    // if night
     currentAstro.textContent = sunrise;
     pastAstro.textContent = sunset;
     sunriseText.textContent = "SUNRISE";
@@ -127,29 +123,24 @@ function fetchAstro(data) {
     barIcon.setAttribute('src', '../images/moon.png');
     barIcon.setAttribute('alt', 'moong icon');
   }
-
   // displays data for 'FEELS LIKE' temperature and wind speed.
-  document.getElementById(
-    "feels-like-temp"
-  ).textContent = `${data.current.feelslike_c}°`;
-  document.getElementById(
-    "wind-speed"
-  ).textContent = `${data.current.wind_kph}`;
+  document.getElementById("feels-like-temp").textContent = `${data.current.feelslike_c}°`;
+  document.getElementById("wind-speed").textContent = `${data.current.wind_kph}`;
 }
-
 // pulls data from api for 3 day forecast
 function forecastData(data) {
   const forecastWrapper = document.getElementById("forecast-wrapper");
   let forecastPath = data.forecast.forecastday;
   // loops through forecast data array and craetes: Hour, Icon, Temperature in HTML format, then appends it in forecast wrapper.
   for (let i = 0; i < forecastPath.length; i++) {
+    // forecast data path
     let iconPath = forecastPath[i].day.condition.icon;
     let minPath = Math.floor(forecastPath[i].day.mintemp_c);
     let maxPath = Math.floor(forecastPath[i].day.maxtemp_c);
     let fulldate = new Date();
     fulldate.setDate(fulldate.getDate() + i);
     const currDay = fulldate.toLocaleString("en-us", { weekday: "long" });
-
+    // Creates HTML for forecast and appends it to forecast wrapper
     const root = document.createElement("div");
     root.classList.add("forecast__row", "flex");
     root.innerHTML = `
@@ -170,15 +161,15 @@ function displayMap(key, lat, lon) {
   let Jawg_Terrain = L.tileLayer(
     "https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}",
     {
-      attribution:
-        '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
       maxZoom: 22,
       subdomains: "abcd",
-      accessToken: key,
+      accessToken: key, // MAP KEY HERE
       zoomControl: false
     }
   ).addTo(map);
+  // adds marker on map
   // // map marker
   // let marker = L.marker([lat, lon], {
   //   color: 'red',
@@ -187,115 +178,78 @@ function displayMap(key, lat, lon) {
   //   radius: 500
   // }).addTo(map);
 }
-
 // display rain and visibility data on page
 function rainData(data) {
   const rainText = document.getElementById('chance-of-rain');
   const snowText = document.getElementById('chance-of-snow');
   const visibilityText = document.getElementById('visibility');
   const airQuality = document.getElementById('air-quality');
-
+  // path for data
   let chanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_rain;
   let chanceOfSnow = data.forecast.forecastday[0].day.daily_chance_of_snow;
   let visibilityKm = data.current.vis_km;
   let airQualityIndex = data.current.air_quality['us-epa-index'];
-
-  console.log(airQualityIndex);
-
+  // air quality text according to its quality
   if(airQualityIndex == '1') airQuality.textContent = 'Good';
   else if(airQualityIndex == '2') airQuality.textContent = 'Moderate';
   else if(airQualityIndex >= '3' && airQualityIndex <= '4') airQuality.textContent = 'Unhealthy';
   else if(airQualityIndex == '5') airQuality.textContent = 'Too unhealthy';
   else if(airQualityIndex == '6') airQuality.textContent = 'Hazardous';
-
+  // insert data in HTML
   rainText.textContent = `${chanceOfRain}%`
   snowText.textContent = `: ${chanceOfSnow}%`
   visibilityText.textContent = `${visibilityKm} km`
-
 }
-
 // change background image of wrapper according to weather condition
 function changeBackground(data) {
   const wrapper = document.getElementById('wrapper');
   // let conditionDay = data.current.condition.icon.split('/')[5];
   let conditionDay = 'night';
   let condition = data.current.condition.icon.split('/')[6].split('.')[0];
-
   // check weather condition and whether it is day or night, then display background image
   if(condition == 113) {
     // background image sunny
-    if(cdayonditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/sunny.jpg)';
-    } else {
-      wrapper.style.background = 'url(./images/night/clear.jpg)';
-    }
+    if(cdayonditionDay === 'day') wrapper.style.background = 'url(./images/day/sunny.jpg)';
+    else wrapper.style.background = 'url(./images/night/clear.jpg)';
   } else if (condition >= 116 && condition <= 122) {
     // CLOUDY CONDITIONS
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/cloudy.jpg)';
-    } else {
-      wrapper.style.background = 'url(./images/night/cloudy.jpg)';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/cloudy.jpg)';
+    else wrapper.style.background = 'url(./images/night/cloudy.jpg)';
   } else if (condition == 143 || condition == 248 || condition == 260) {
     // background image mist
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/mist.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/mist.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/mist.jpg';
+    else wrapper.style.background = 'url(./images/night/mist.jpg';
   } else if (condition == 176 || condition >= 293 && condition <= 302 || condition == 311 || condition == 353 || condition == 185 || condition >= 263 && condition <= 284 || condition == 317 || condition == 320) {
     // background image light/medium rain
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/rain.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/heavy_rain.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/rain.jpg';
+    else wrapper.style.background = 'url(./images/night/heavy_rain.jpg';
   } else if(condition >= 305 && condition <= 308 || condition ===314 || condition == 356 || condition == 359 || condition == 182) {
     // background image heavy rain
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/heavy_rain.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/heavy_rain.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/heavy_rain.jpg';
+    else wrapper.style.background = 'url(./images/night/heavy_rain.jpg';
   } else if(condition == 386 || condition == 389) {
     // background image rain with thunder
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/light_thunder.jpg)';
-    } else {
-      wrapper.style.background = 'url(./images/night/light_thunder.jpg)';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/light_thunder.jpg)';
+    else wrapper.style.background = 'url(./images/night/light_thunder.jpg)';
   } else if(condition == 200) {
     // thundery outbreak image
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/thundery_outbreak.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/thundery_outbreak.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/thundery_outbreak.jpg';
+    else wrapper.style.background = 'url(./images/night/thundery_outbreak.jpg';
   } else if(condition == 179 || condition >= 323 && condition <= 329 || condition >= 362 && condition <= 368 || condition == 392) {
     // light snow
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/light_snow.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/light_snow.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/light_snow.jpg';
+    else wrapper.style.background = 'url(./images/night/light_snow.jpg';
   } else if(condition == 227 || condition == 230 || condition >= 332 && condition <= 338 || condition == 371 || condition == 395) {
     // heavy snow
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/heavy_snow.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/heavy_snow.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/heavy_snow.jpg';
+    else wrapper.style.background = 'url(./images/night/heavy_snow.jpg';
   } else if(condition == 350 || condition == 374 || condition == 377) {
     // ice palletes
-    if(conditionDay === 'day') {
-      wrapper.style.background = 'url(./images/day/ice_pellete.jpg';
-    } else {
-      wrapper.style.background = 'url(./images/night/ice_pellete.jpg';
-    }
+    if(conditionDay === 'day') wrapper.style.background = 'url(./images/day/ice_pellete.jpg';
+    else wrapper.style.background = 'url(./images/night/ice_pellete.jpg';
   }
-
+  // center background image
   wrapper.style.backgroundPosition = 'center';
   wrapper.style.backgroundRepeat = 'no-repeat';
   wrapper.style.backgroundSize = 'cover';
-
 }
